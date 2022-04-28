f1d24df40dd58d8e32afed7f48ef23f23bd31c1a
Ann Francis Ocaso <ocasoannfrancis100202@gmail.com>
2022-04-28 22:12:16 +0800

    first commit

---
+++ MSGINABOX/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSGINABOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi, I am Ann Francis C. Ocaso ;>", ("Of course you want!"));
+        }
+    }
+}
